c7991bc7ab83a0ba7c1ed8f3a2ef9d436e70ee1b
Gustavo "Goose" Grau Bolivar <gustavo.graubolivar@kellogg.northwestern.edu>
2023-04-14 23:11:36 +0000

    Created forst pass of models and insert of movies

--- kmdb.sql
+++ kmdb.sql
@@ -104,14 +104,45 @@
 .headers off
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
--- TODO!
+
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS studios;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS castings;
 
 -- Create new tables, according to your domain model
--- TODO!
+
+CREATE TABLE movies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    year_released INTEGER,
+    MPAA_rating TEXT,
+    studio TEXT
+);
+
+CREATE TABLE actors (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+
+CREATE TABLE castings (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    actor_id INTEGER,
+    movie_id INTEGER,
+    character_name
+);
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
--- TODO!
+
+INSERT INTO movies (title,year_released,MPAA_rating,studio)
+VALUES  ("Batman Begins",2005,"PG-13","Warner Bros.");
+
+INSERT INTO movies (title,year_released,MPAA_rating,studio)
+VALUES  ("The Dark Knight",2008,"PG-13","Warner Bros.");
+
+INSERT INTO movies (title,year_released,MPAA_rating,studio)
+VALUES  ("The Dark Knight Rises",2012,"PG-13","Warner Bros.");
 
 -- Prints a header for the movies output
 .print "Movies"
@@ -119,7 +150,8 @@
 .print ""
 
 -- The SQL statement for the movies output
--- TODO!
+
+SELECT title, year_released, MPAA_rating, studio FROM movies;
 
 -- Prints a header for the cast output
 .print ""
